16668295ce2849729f8c969f056198e7387e1519
Arashpreet-Kaur <157736771+Arashpreet-Kaur@users.noreply.github.com>
2025-06-15 00:18:38 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/newton', function(req, res, next) {
+  res.render('newton');
+});
+
+router.get('/einstein', function(req, res, next) {
+  res.render('einstein');
+});
+
+router.get('/curie', function(req, res, next) {
+  res.render('curie');
+});
+
+router.get('/tesla', function(req, res, next) {
+  res.render('tesla');
 });
 
 module.exports = router;
